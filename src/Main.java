fec0287589bb3535aa7c1bbf5db1314d544ec9f4
Elena <laaarsen@mail.ru>
2023-01-24 02:00:19 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10000;
+        byte a = 20;
+        int o = (x / a);
+        int m = (o / a);
+
+
+        System.out.println("Начислено миль " + m);
+    }
+}
